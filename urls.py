3fa899a24825feccbf1cc27bf69819a1bd5f919e
pastmax4 <46377444+pastmax4@users.noreply.github.com>
2019-05-02 15:59:52 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import  home_view
+from pages.views import  about_view
 
 urlpatterns = [
     path('', home_view  , name='home'),
+    path('about/', about_view),
     path('admin/', admin.site.urls),
 ]
-
