f8196a34355b72e778520d38650d7d7afd7da9d2
pastmax4 <46377444+pastmax4@users.noreply.github.com>
2019-05-02 16:01:52 +0200

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 
+
 #from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -10,3 +11,11 @@ def home_view(request,*args,**kwargs ):
     #return HttpResponse("<h1> Hello from world from massimo. </h1> ")
     return render(request,"home.html",{})
 
+def about_view(request,*args,**kwargs ):
+    my_context = {
+        "my_text": "This is about us.",
+        "my_number": 1234,
+        "my_list": ['Ciao','Bravo','Bene']
+
+    }
+    return render(request, "about.html", my_context )
