1e36af9cfa780df2dc0634ffae738dbbd6a5aa8e
pastmax4 <46377444+pastmax4@users.noreply.github.com>
2019-05-08 17:18:57 +0200

    Add files via upload

---
+++ trydjango20190508/pages/views.py
@@ -0,0 +1,22 @@
+
+
+#from django.template import loader
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# 30apr2019 massimo
+# Create your views here.
+def home_view(request,*args,**kwargs ):
+    #return HttpResponse("<h1> Hello from world from massimo. </h1> ")
+    return render(request,"home.html",{})
+
+def about_view(request,*args,**kwargs ):
+    my_context = {
+        "title":"prova djangoooo.",
+        "my_text": "This is about us.",
+        "my_number": "1234",
+        "my_list": ['Ciao','Bravo','Bene','100']
+
+    }
+    return render(request, "about.html", my_context )
---
+++ trydjango20190508/products/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from .models import Product
+
+# Create your views here.
+def product_detail_view(request ):
+    obj = Product.objects.get(id=1)
+
+
+    context = {'title': obj.title ,
+               'description': obj.description
+               }
+
+    return render(request,"product/detail.html",context)
+
+
+
+#def about_view(request,*args,**kwargs ):
